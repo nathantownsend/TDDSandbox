f7e3a8c4c6fc58e771a6a312c19e842b27d45de6
Nathan Townsend <ntownsend2@mt.gov>
2014-12-19 12:58:00 -0700

    Setup NUnit and an example test class

---
+++ UnitTesting/ExampleTest.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace TailSpinToys.UnitTesting
+{
+
+    [TestFixture]
+    public class NUnitTests
+    {
+
+        [Test]
+        public void SumOfTwoNumbers()
+        {
+
+            Assert.AreEqual(10, 5 + 5);
+
+        }
+
+
+
+        [Test]
+        public void AreTheValuesTheSame()
+        {
+
+            Assert.AreSame(10, 5 + 6);
+
+        }
+
+    }
+
+}
